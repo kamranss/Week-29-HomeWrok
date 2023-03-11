16be2e5af025b420c665b1509282472328f69fd2
kamranss <kamranss@code.edu.az>
2023-03-11 15:21:49 +0400

    Refactor: task updated

--- assets/script/drag-drop.js
+++ assets/script/drag-drop.js
@@ -244,3 +244,5 @@ function DeleteButton(deleteAll){
 
         })
 }
+
+
---
+++ assets/script/practiseClass.js
@@ -0,0 +1,11 @@
+class Person {
+    name ="kamran"
+    surname = "suleymanov"
+
+
+}
+
+let newPerson = new Person();
+
+newPerson.name = "Elman";
+console.log(newPerson.name);
